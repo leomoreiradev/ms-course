97b586b17008c75cccfb6d0e264336d97360a22c
LEANDRO MOREIRA <leandrodev.oliveira@gmail.com>
2021-12-31 17:10:50 -0300

    Feign Cloud

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,23 @@
+package com.devsuperior.hrpayroll.feignclients;
+
+
+import com.devsuperior.hrpayroll.entities.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/*
+* @Component = Componente gerenciado pelo spring para poder injeta-lo em outro lugar
+* @FeignClient(name = "nome do serviço a ser chamado", url = "url do serviço", path = "/workers")
+* */
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+
+    //Tipo de chamada que vai ser feita ao webservice
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Worker> findById(@PathVariable Long id);
+}
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -2,35 +2,21 @@ package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    //Uri da requisição que está no application.properties
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-
-        //Criando as variaveis par a arequisição
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId) );
-
-        //Fazendo a requisição via restTemplate
-        //Montando a url workerHost + "/workers/{id}", tipo do obj , varivaveis da requisição
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
-
-        return new Payment(worker.getName(), worker.getDailyIncome(), days );
+        //Fazendo a requisição via feingClient
+        //workerFeignClient.findById(workerId).getBody(), o getBody() pega o obj dentro do ResponseEntity
+        Worker worker = workerFeignClient.findById(workerId).getBody();
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
