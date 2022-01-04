aba512ad498bfabf14ac9a0de81dbbce3ed65859
LEANDRO MOREIRA <leandrodev.oliveira@gmail.com>
2022-01-03 23:51:51 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/feignclients/WorkerFeignClient.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 * */
 
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
 
     //Tipo de chamada que vai ser feita ao webservice
